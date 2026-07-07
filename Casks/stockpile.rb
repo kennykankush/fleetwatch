e6131565a7090f7417b39360628b8f3f69124c4c
kennykankush <kennykankush@gmail.com>
2026-07-07 13:47:14 +0800

    cask: v0.1.3

--- Casks/stockpile.rb
+++ Casks/stockpile.rb
@@ -1,6 +1,6 @@
 cask "stockpile" do
-  version "0.1.2"
-  sha256 "62da8f1415b70004338ca22245cab72cd4bc940aec3873d06425cbac362ee355"
+  version "0.1.3"
+  sha256 "b945a9add9fb750ac780beadc19184a5311a19c988c2f46033ceaf4cebf37460"
 
   url "https://github.com/kennykankush/stockpile/releases/download/v#{version}/Stockpile-#{version}.zip"
   name "Stockpile"
