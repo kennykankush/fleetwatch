a8c60e7fbe3988b4cf76196e015f14e007fd1f2c
kennykankush <kennykankush@gmail.com>
2026-09-23 00:18:33 +0800

    cask: v0.6.0

--- Casks/fleetwatch.rb
+++ Casks/fleetwatch.rb
@@ -1,6 +1,6 @@
 cask "fleetwatch" do
-  version "0.5.0"
-  sha256 "ee564f3fc08e264aaf0e27134df78c8b350728e43d69e71304afb786a1332f01"
+  version "0.6.0"
+  sha256 "ca315421dfab67d2880492b73e55573f7d2e8f93b0c29ae0eb0b60fe784ed5a1"
 
   url "https://github.com/kennykankush/fleetwatch/releases/download/v#{version}/Fleetwatch-#{version}.zip"
   name "Fleetwatch"
